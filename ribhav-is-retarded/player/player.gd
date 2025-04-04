c1a31b3663626b3f4432c759cfb83e014a3182bf
Cxntext <joshua.xy.wang@gmail.com>
2025-04-03 22:14:10 -0700

    added walking animation

--- ribhav-is-retarded/player/player.gd
+++ ribhav-is-retarded/player/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-
+var anim_player: AnimationPlayer
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @export var inv: Inv
-
+func _ready():
+	anim_player = $AnimationPlayer
+	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
@@ -18,6 +20,13 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
+	if Input.is_action_pressed("Right"):
+		anim_player.play("walk_right")
+	if Input.is_action_pressed("Left"):
+		anim_player.play("walk_right", -1)
+	if Input.is_action_just_released("Right") or Input.is_action_just_released("Left"):
+		anim_player.stop()
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
